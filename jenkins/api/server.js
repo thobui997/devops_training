6df97291ce6c8bd47a01dbc3e74b0fb4ddbc2d36
ThoBT <thob2105@gmail.com>
2022-06-05 21:54:25 +0700

    add jenkins

---
+++ jenkins/api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/api/hello', function (req, res) {
+	res.send('Hello World, I am API');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server started on port: ${PORT}`);
+});
