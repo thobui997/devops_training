8e6fd151378122522358d41b40627019b46f47c1
ThoBT <thob2105@gmail.com>
2022-06-06 00:24:47 +0700

    test reponse

--- jenkins/api/server.js
+++ jenkins/api/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 app.get('/api/hello', function (req, res) {
-	res.send('Hello World, I am API');
+	res.send('Hello World, I am API with Jenkins');
 });
 
 app.listen(PORT, () => {
